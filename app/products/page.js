2c12ad4fc1e5872d75195d4a90682d77b65e5493
MuratOfficial <toimetm@gmail.com>
2025-11-04 21:38:46 +0500

    init

---
+++ app/products/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ function Products() {
+  return (
+    <div className=' w-full h-screen bg-gray-500 text-5xl text-white'>Products</div>
+  )
+}
+
+export default Products
+
+/// href="/products"
+
+
+/// billboard
+//// basket
+/// catalogs
+
+//rfce 
