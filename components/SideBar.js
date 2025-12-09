8c3744b50e81246e88af301ee6d61c4db62483a4
MuratOfficial <toimetm@gmail.com>
2025-12-09 19:24:01 +0500

    added useEffect to SideBar

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,12 +1,34 @@
 "use client"
 
-import { bigButtonsFemale, categories, picks, tags } from '@/lib/data';
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
 import { LuArrowRight } from "react-icons/lu";
 
 function SideBar({ isOpen, onClose }) {
 
+    const [bigButtonsFemale, setBigButtonFemale] = useState([]);
+    const [categories, setCategories] = useState([]);
 
+
+    useEffect(()=>{
+        async function getData(){
+            const res = await fetch("http://localhost:3001/categories",{
+                method:"POST"
+            })
+            if(!res.ok){
+                console.log("Данные не подгрузились")
+            } else{
+                const data = await res.json();
+                setBigButtonFemale(data.slice(0, 4));
+                setCategories(data.slice(4))
+            }
+        }
+
+
+        getData();
+
+
+    }, [])
 
     return (
         <div 
@@ -22,7 +44,7 @@ function SideBar({ isOpen, onClose }) {
             </div>
 
             <div className='flex flex-col gap-4 items-start px-8 py-4 border-b border-black/10'>
-                {bigButtonsFemale.map((x, index)=>(
+                {bigButtonsFemale?.map((x, index)=>(
                     <a key={index} href={`/catalogs/${x.path}`} className='text-2xl hover:opacity-80 cursor-pointer group uppercase flex flex-row gap-1 items-center relative'> 
                         <LuArrowRight className='w-0 group-hover:w-5 transition-all duration-400' size={18}/> 
                         <span className='transition-all duration-400'>{x.name}</span> 
@@ -31,7 +53,7 @@ function SideBar({ isOpen, onClose }) {
             </div>
             
             <div className='flex flex-col gap-2 items-start px-8 py-4 border-b border-black/10'>
-                {categories.map((x, index)=>(
+                {categories?.map((x, index)=>(
                     <a href={`/catalogs/${x.path}`} key={index} className='hover:opacity-80 text-xs uppercase'>{x.name}</a>
                 ))}
             </div>
@@ -41,9 +63,9 @@ function SideBar({ isOpen, onClose }) {
                     Ұсынылды
                 </p>
                 <a href='#' className='hover:opacity-80 text-red-500 text-xs uppercase'>Акциялар</a>
-                {picks.map((x, index)=>(
+                {/* {picks.map((x, index)=>(
                     <a href={`/catalogs/${x.path}`} key={index} className='hover:opacity-80 text-xs uppercase'>{x.name}</a>
-                ))}
+                ))} */}
             </div>
 
             <div className='flex flex-col gap-2 items-start px-8 py-4 border-b border-black/10'>
@@ -53,12 +75,12 @@ function SideBar({ isOpen, onClose }) {
                 <a href={`/catalogs/music`} className='hover:opacity-80 text-xs uppercase'>Музыка</a>
                 <a href="/catalogs/personalization" className='hover:opacity-80 text-xs uppercase'>Жекелендіру</a>
             </div>
-
+{/* 
             <div className='flex flex-row gap-2 items-start px-8 py-4 border-b border-black/10'>
                 {tags.map((x, index)=>(
                     <a href={`/catalogs/${x.path}`} key={index} className='hover:opacity-80 text-xs uppercase py-1 px-2 border border-black/10 rounded-full'>{x.name}</a>
                 ))}
-            </div>
+            </div> */}
         </div>
     )
 }
